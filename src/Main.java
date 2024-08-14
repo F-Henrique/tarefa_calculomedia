458d23a224d1adf9277f76d40658428d8775deb4
F-Henrique <santos.rique89@gmail.com>
2024-08-13 22:39:54 -0300

    arquivos do intellij salvos

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        double nota1 = 1.0;
+        double nota2 = 2.0;
+
+        double nota3 = 1.0;
+        double nota4 = 5.0;
+
+        double media = (nota1+nota2+nota3+nota4)/4;
+
+        System.out.printf("A média é: " + String.valueOf(media));
+
+    }
+}
